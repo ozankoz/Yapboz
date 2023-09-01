701b244a0cc59b51d584a31ee5ff90090c8a147b
ok250030 <ok250030@WIN-CMBZCS3>
2023-09-01 11:59:42 +0300

    Add project files.

---
+++ Yapboz/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Yapboz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.btnKaristir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbSeviye = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ofdResimSec = new System.Windows.Forms.OpenFileDialog();
+            this.btnResimSec = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.Enabled = false;
+            this.panelBoard.Location = new System.Drawing.Point(12, 12);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(410, 410);
+            this.panelBoard.TabIndex = 2;
+            // 
+            // btnKaristir
+            // 
+            this.btnKaristir.Enabled = false;
+            this.btnKaristir.Location = new System.Drawing.Point(458, 81);
+            this.btnKaristir.Name = "btnKaristir";
+            this.btnKaristir.Size = new System.Drawing.Size(75, 23);
+            this.btnKaristir.TabIndex = 3;
+            this.btnKaristir.Text = "Karıştır";
+            this.btnKaristir.UseVisualStyleBackColor = true;
+            this.btnKaristir.Click += new System.EventHandler(this.btnKaristir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(455, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Seviye";
+            // 
+            // cbSeviye
+            // 
+            this.cbSeviye.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSeviye.FormattingEnabled = true;
+            this.cbSeviye.Items.AddRange(new object[] {
+            "Seviye 1",
+            "Seviye 2",
+            "Seviye 3",
+            "Seviye 4"});
+            this.cbSeviye.Location = new System.Drawing.Point(458, 54);
+            this.cbSeviye.Name = "cbSeviye";
+            this.cbSeviye.Size = new System.Drawing.Size(75, 21);
+            this.cbSeviye.TabIndex = 5;
+            this.cbSeviye.SelectedIndexChanged += new System.EventHandler(this.cbSeviye_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(435, 312);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnResimSec
+            // 
+            this.btnResimSec.Location = new System.Drawing.Point(458, 12);
+            this.btnResimSec.Name = "btnResimSec";
+            this.btnResimSec.Size = new System.Drawing.Size(75, 23);
+            this.btnResimSec.TabIndex = 7;
+            this.btnResimSec.Text = "Resim Seç";
+            this.btnResimSec.UseVisualStyleBackColor = true;
+            this.btnResimSec.Click += new System.EventHandler(this.btnResimSec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 438);
+            this.Controls.Add(this.btnResimSec);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbSeviye);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKaristir);
+            this.Controls.Add(this.panelBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Yapboz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.Button btnKaristir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbSeviye;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog ofdResimSec;
+        private System.Windows.Forms.Button btnResimSec;
+    }
+}
+
---
+++ Yapboz/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Yapboz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int farex, farey = 0;
+        int seviye = 100;
+        int parca = 16;
+        int[] seviyeler = { 100, 80, 50, 40 };
+        string dosyaKonum = string.Empty;
+        private void ResimleriDiz()
+        {
+            panelBoard.Controls.Clear();
+            int iks = 0;
+            int ye = 0;
+            for (int i = 1; i < parca + 1; i++)
+            {
+                Label lbl = new Label();
+                lbl.Name = i.ToString();
+                lbl.Tag = i.ToString();
+                lbl.BackgroundImage = Image.FromFile(Application.StartupPath + @"\photo\" + cbSeviye.SelectedItem.ToString() + " " + i + ".jpg");
+                lbl.Size = new System.Drawing.Size(seviye, seviye);
+                lbl.Margin = new System.Windows.Forms.Padding(1);
+                lbl.Location = new Point(iks, ye);
+                lbl.BorderStyle = BorderStyle.Fixed3D;
+                lbl.MouseUp += lbl_MouseUp;
+                iks += seviye;
+                if (iks >= 400)
+                {
+                    iks = 0;
+                }
+                ye = (i / Convert.ToInt32(Math.Sqrt(parca))) * seviye;
+                panelBoard.Controls.Add(lbl);
+            }
+            pictureBox1.Image = Image.FromFile(dosyaKonum);
+            btnKaristir.Enabled = true;
+        }
+
+        void lbl_MouseUp(object sender, MouseEventArgs e)
+        {
+            Label lbl = sender as Label;
+            ArrayList labelcevresi = new ArrayList();
+            int boslabel = 0;
+            int satir = Convert.ToInt32(lbl.Tag) / Convert.ToInt32(Math.Sqrt(parca));
+            int sutun = Convert.ToInt32(lbl.Tag) - (Convert.ToInt32(Math.Sqrt(parca)) * satir);
+            if (sutun == 0)
+            {
+                sutun = Convert.ToInt32(Math.Sqrt(parca));
+                satir = satir - 1;
+            }
+            for (int i = sutun - 1; i > 0; i--)
+            {
+                labelcevresi.Add(i + (Convert.ToInt32(Math.Sqrt(parca)) * satir));
+            }
+            for (int i = sutun + 1; i < Convert.ToInt32(Math.Sqrt(parca)) + 1; i++)
+            {
+                labelcevresi.Add(i + (Convert.ToInt32(Math.Sqrt(parca)) * satir));
+            }
+            for (int i = satir - 1; i > -1; i--)
+            {
+                labelcevresi.Add((i * Convert.ToInt32(Math.Sqrt(parca))) + sutun);
+            }
+            for (int i = satir + 1; i < Convert.ToInt32(Math.Sqrt(parca)); i++)
+            {
+                labelcevresi.Add((i * Convert.ToInt32(Math.Sqrt(parca))) + sutun);
+            }
+            foreach (Label item in panelBoard.Controls)
+            {
+                if (item.BackgroundImage == null)
+                {
+                    boslabel = Convert.ToInt32(item.Tag);
+                    int gidilecekkareadedi = (Convert.ToInt32(lbl.Tag) - boslabel) * -1;
+                }
+            }
+            foreach (int cevrelabel in labelcevresi)
+            {
+                if (cevrelabel == boslabel)
+                {
+                    int gidilecekkareadedi = (Convert.ToInt32(lbl.Tag) - boslabel) * -1;
+                    if (gidilecekkareadedi > 0 && gidilecekkareadedi < Convert.ToInt32(Math.Sqrt(parca)))
+                    {
+                        for (int i = gidilecekkareadedi; i > -1; i--)
+                        {
+                            foreach (Label item in panelBoard.Controls)
+                            {
+                                if (Convert.ToInt32(item.Tag) == Convert.ToInt32(lbl.Tag) + i)
+                                {
+                                    foreach (Label mitem in panelBoard.Controls)
+                                    {
+                                        if (mitem.BackgroundImage == null)
+                                        {
+                                            mitem.Location = new Point(mitem.Location.X - (gidilecekkareadedi * mitem.Height), mitem.Location.Y);
+                                            mitem.Tag = boslabel - gidilecekkareadedi;
+                                        }
+                                    }
+                                    item.Location = new Point(item.Location.X + item.Height, item.Location.Y);
+                                    item.Tag = Convert.ToInt32(item.Tag) + 1;
+                                }
+                            }
+                        }
+                    }
+                    if (gidilecekkareadedi < 0 && gidilecekkareadedi > Convert.ToInt32(Math.Sqrt(parca) * -1))
+                    {
+                        for (int i = gidilecekkareadedi; i < 1; i++)
+                        {
+                            foreach (Label item in panelBoard.Controls)
+                            {
+                                if (Convert.ToInt32(item.Tag) == Convert.ToInt32(lbl.Tag) + i)
+                                {
+                                    foreach (Label mitem in panelBoard.Controls)
+                                    {
+                                        if (mitem.BackgroundImage == null)
+                                        {
+                                            mitem.Location = new Point(mitem.Location.X + (gidilecekkareadedi * mitem.Height * -1), mitem.Location.Y);
+                                            mitem.Tag = boslabel - gidilecekkareadedi;
+                                        }
+                                    }
+                                    item.Location = new Point(item.Location.X - item.Height, item.Location.Y);
+                                    item.Tag = Convert.ToInt32(item.Tag) - 1;
+                                }
+                            }
+                        }
+                    }
+                    if (gidilecekkareadedi <= Convert.ToInt32(Math.Sqrt(parca) * -1))
+                    {
+                        for (int i = -1; i > gidilecekkareadedi / Convert.ToInt32(Math.Sqrt(parca)) - 1; i--)
+                        {
+                            foreach (Label item in panelBoard.Controls)
+                            {
+                                if (Convert.ToInt32(item.Tag) == Convert.ToInt32(lbl.Tag) + gidilecekkareadedi + (Convert.ToInt32(Math.Sqrt(parca)) * i * -1))
+                                {
+                                    foreach (Label mitem in panelBoard.Controls)
+                                    {
+                                        if (mitem.BackgroundImage == null)
+                                        {
+                                            mitem.Location = new Point(mitem.Location.X, mitem.Location.Y + mitem.Height * -1);
+                                            mitem.Tag = boslabel - gidilecekkareadedi;
+                                        }
+                                    }
+                                    item.Location = new Point(item.Location.X, item.Location.Y - item.Height);
+                                    item.Tag = Convert.ToInt32(item.Tag) - Convert.ToInt32(Math.Sqrt(parca));
+                                }
+                            }
+                        }
+                    }
+                    if (gidilecekkareadedi >= Convert.ToInt32(Math.Sqrt(parca)))
+                    {
+                        for (int i = 1; i < gidilecekkareadedi / Convert.ToInt32(Math.Sqrt(parca)) + 1; i++)
+                        {
+                            foreach (Label item in panelBoard.Controls)
+                            {
+                                if (Convert.ToInt32(item.Tag) == Convert.ToInt32(lbl.Tag) + gidilecekkareadedi + (Convert.ToInt32(Math.Sqrt(parca)) * i * -1))
+                                {
+                                    foreach (Label mitem in panelBoard.Controls)
+                                    {
+                                        if (mitem.BackgroundImage == null)
+                                        {
+                                            mitem.Location = new Point(mitem.Location.X, mitem.Location.Y - mitem.Height);
+                                            mitem.Tag = boslabel - gidilecekkareadedi;
+                                        }
+                                    }
+                                    item.Location = new Point(item.Location.X, item.Location.Y + item.Height);
+                                    item.Tag = Convert.ToInt32(item.Tag) + Convert.ToInt32(Math.Sqrt(parca));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private Image Crop(Image img, Rectangle rect)
+        {
+            Bitmap bmpOrj = new Bitmap(img);
+            Bitmap bmpCrop = bmpOrj.Clone(rect, bmpOrj.PixelFormat);
+            return (Image)(bmpCrop);
+        }
+
+        private void btnKaristir_Click(object sender, EventArgs e)
+        {
+            panelBoard.Enabled = true;
+            ArrayList siralama = new ArrayList();
+            foreach (var label in panelBoard.Controls)
+            {
+                if (label is Label)
+                {
+                    Label lbl = label as Label;
+                    if (Convert.ToInt32(lbl.Name) == parca)
+                    {
+                        lbl.BackgroundImage = null;
+                        lbl.BorderStyle = BorderStyle.None;
+                    }
+                }
+            }
+            for (int i = 1; i < parca + 1; i++)
+            {
+                siralama.Add(i);
+            }
+            shuffle(siralama);
+            int iks = 0;
+            int ye = 0;
+            int kacinci = 0;
+            foreach (int item in siralama)
+            {
+                foreach (Label lbl in panelBoard.Controls)
+                {
+                    if (item == Convert.ToInt32(lbl.Name))
+                    {
+                        kacinci++;
+                        lbl.Tag = kacinci;
+                        lbl.Location = new Point(iks, ye);
+                        iks += seviye;
+                        if (iks >= 400)
+                        {
+                            iks = 0;
+                        }
+                        ye = (kacinci / Convert.ToInt32(Math.Sqrt(parca))) * seviye;
+                    }
+                }
+
+            }
+        }
+        public static void shuffle(ArrayList array)
+        {
+            Random rng = new Random(); // i.e., java.util.Random.
+            int n = array.Count; // The number of items left to shuffle (loop invariant).
+            while (n > 1)
+            {
+                int k = rng.Next(n); // 0 <= k < n.
+                n--; // n is now the last pertinent index;
+                int temp = Convert.ToInt32(array[n]); // swap array[n] with array[k] (does nothing if k == n).
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbSeviye_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbSeviye.SelectedIndex == 0)
+            {
+                seviye = 100;
+                parca = 16;
+            }
+            else if (cbSeviye.SelectedIndex == 1)
+            {
+                seviye = 80;
+                parca = 25;
+            }
+            else if (cbSeviye.SelectedIndex == 2)
+            {
+                seviye = 50;
+                parca = 64;
+            }
+            else
+            {
+                seviye = 40;
+                parca = 100;
+            }
+            ResimleriDiz();
+        }
+
+        private void btnResimSec_Click(object sender, EventArgs e)
+        {
+            ofdResimSec.Filter = "*.jpeg, *.jpg)|*.jpeg;*.jpg";
+            ofdResimSec.ShowDialog();
+            dosyaKonum = ofdResimSec.FileName;
+            if (dosyaKonum == "")
+            {
+                return;
+            }
+            btnResimSec.Enabled = false;
+            //if (File.Exists(Application.StartupPath + @"\photo.jpg"))
+            //{
+            //    dosyaKonum = Application.StartupPath + @"\photo.jpg";
+            //}
+            //else if (File.Exists(Application.StartupPath + @"\photo.jpeg"))
+            //{
+            //    dosyaKonum = Application.StartupPath + @"\photo.jpeg";
+            //}
+            //else
+            //{
+            //    ofdResimSec.Filter = "*.jpeg, *.jpg)|*.jpeg;*.jpg";
+            //    ofdResimSec.ShowDialog();
+            //    dosyaKonum = ofdResimSec.FileName;
+            //    this.BringToFront();
+            //}
+            //try
+            //{
+            //    File.Copy(dosyaKonum, Application.StartupPath + "\\photothump.jpg", true);
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Bu resim başka bir işlemde kullanılıyor.");
+            //    return;
+            //}
+            //File.Delete(dosyaKonum);
+            //dosyaKonum = Application.StartupPath + "\\photothump.jpg";
+            Image anaResim = Image.FromFile(dosyaKonum);
+            int kare = seviye;
+            if (anaResim.Height % kare != 0 || anaResim.Width % kare != 0)
+            {
+                MessageBox.Show("Resim bu ölçülerde boyutlandırılmaz.");
+                Application.Exit();
+            }
+            else if (anaResim.Height > 400)
+            {
+                Bitmap yeniresim = new Bitmap(anaResim, 400, 400);
+                anaResim = yeniresim;
+            }
+            else if (anaResim.Height < 400)
+            {
+                MessageBox.Show("Resim boyutları çok küçük, lütfen başka bir resim seçiniz.");
+                Application.Exit();
+            }
+            int parcanumarasi = 0;
+            int seviyesirasi = 0;
+            foreach (var item in cbSeviye.Items)
+            {
+                kare = seviyeler[seviyesirasi];
+                int dikeyParca = anaResim.Height / kare;
+                int yatayParca = anaResim.Width / kare;
+                Rectangle cropAlani = new Rectangle(0, 0, kare, kare);
+                for (int i = 0; i < dikeyParca; i++)
+                {
+                    for (int y = 0; y < yatayParca; y++)
+                    {
+                        cropAlani.Y = i * kare;
+                        cropAlani.X = y * kare;
+                        Image parcaResim = Crop(anaResim, cropAlani);
+                        if (Directory.Exists(Application.StartupPath + @"\photo\") == false)
+                        {
+                            Directory.CreateDirectory(Application.StartupPath + @"\photo\");
+                        }
+                        parcanumarasi++;
+                        parcaResim.Save(Application.StartupPath + @"\photo\" + item.ToString() + " " + parcanumarasi + ".jpg", ImageFormat.Jpeg);
+                    }
+                }
+                parcanumarasi = 0;
+                seviyesirasi++;
+            }
+            cbSeviye.SelectedIndex = 0;
+        }
+    }
+}
